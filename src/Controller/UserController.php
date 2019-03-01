be417b6bd651e2c35297d0fc2d1c21f4837a7130
Jakub <jaresz.git@listy.internetowe.pl>
2019-03-02 00:47:00 +0100

    only if user - fix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,115 +17,115 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserController extends Controller
 {
 
-	/**
-	 * @Route("/", name="user_index", methods="GET")
-	 */
-	public function index(UserRepository $userRepository): Response
-	{
-		return $this->render('user/index.html.twig', [
-			'users' => $userRepository->findAll()
-		]);
-	}
+    /**
+     * @Route("/", name="user_index", methods="GET")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll()
+        ]);
+    }
 
-	/**
-	 * @Route("/new", name="user_new", methods="GET|POST")
-	 */
-	public function new(Request $request): Response
-	{
-		$user = new User();
-		$form = $this->createForm(UserType::class, $user);
-		$form->handleRequest($request);
-		
-		if ($form->isSubmitted() && $form->isValid()) {
-			$em = $this->getDoctrine()->getManager();
-			$em->persist($user);
-			$em->flush();
-			
-			return $this->redirectToRoute('user_index');
-		}
-		
-		return $this->render('user/new.html.twig', [
-			'user' => $user,
-			'form' => $form->createView()
-		]);
-	}
+    /**
+     * @Route("/new", name="user_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            
+            return $this->redirectToRoute('user_index');
+        }
+        
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}", name="user_show", methods="GET")
-	 */
-	public function show(User $user): Response
-	{
-		return $this->render('user/show.html.twig', [
-			'user' => $user
-		]);
-	}
+    /**
+     * @Route("/{id}", name="user_show", methods="GET")
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}/edit", name="user_edit", methods="GET|POST")
-	 */
-	public function edit(Request $request, User $user): Response
-	{
-		$form = $this->createForm(UserType::class, $user);
-		$form->handleRequest($request);
-		
-		if ($form->isSubmitted() && $form->isValid()) {
-			$this->getDoctrine()
-				->getManager()
-				->flush();
-			
-			return $this->redirectToRoute('user_edit', [
-				'id' => $user->getId()
-			]);
-		}
-		
-		return $this->render('user/edit.html.twig', [
-			'user' => $user,
-			'form' => $form->createView()
-		]);
-	}
+    /**
+     * @Route("/{id}/edit", name="user_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()
+                ->getManager()
+                ->flush();
+            
+            return $this->redirectToRoute('user_edit', [
+                'id' => $user->getId()
+            ]);
+        }
+        
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}/change_pass", name="user_change_pass", methods="GET|POST")
-	 */
-	public function changePassword(Request $request, User $user, UserPasswordEncoderInterface $encoder): Response
-	{
-		$form = $this->createForm(UserPassType::class, $user);
-		$form->handleRequest($request);
-		
-		if ($form->isSubmitted() && $form->isValid()) {
-			
-			$plainPassword = $user->getNewPassword();
-			$encoded = $encoder->encodePassword($user, $plainPassword);
-			$user->setPassword($encoded);
-			
-			$this->getDoctrine()
-				->getManager()
-				->flush();
-			
-			$this->addFlash('success', 'Password changed.');
-			
-			return $this->redirectToRoute('user_change_pass', [
-				'id' => $user->getId()
-			]);
-		}
-		
-		return $this->render('user/change_pass.html.twig', [
-			'user' => $user,
-			'form' => $form->createView()
-		]);
-	}
+    /**
+     * @Route("/{id}/change_pass", name="user_change_pass", methods="GET|POST")
+     */
+    public function changePassword(Request $request, User $user, UserPasswordEncoderInterface $encoder): Response
+    {
+        $form = $this->createForm(UserPassType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $user->getNewPassword();
+            $encoded = $encoder->encodePassword($user, $plainPassword);
+            $user->setPassword($encoded);
+            
+            $this->getDoctrine()
+                ->getManager()
+                ->flush();
+            
+            $this->addFlash('success', 'Password changed.');
+            
+            return $this->redirectToRoute('user_change_pass', [
+                'id' => $user->getId()
+            ]);
+        }
+        
+        return $this->render('user/change_pass.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}", name="user_delete", methods="DELETE")
-	 */
-	public function delete(Request $request, User $user): Response
-	{
-		if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
-			$em = $this->getDoctrine()->getManager();
-			$em->remove($user);
-			$em->flush();
-		}
-		
-		return $this->redirectToRoute('user_index');
-	}
+    /**
+     * @Route("/{id}", name="user_delete", methods="DELETE")
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($user) {
+            if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($user);
+                $em->flush();
+            }
+        }
+        return $this->redirectToRoute('user_index');
+    }
 }
